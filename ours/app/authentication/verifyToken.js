00f66e3f4fbcad8415e6d574a91041b29200da4d
kenny281 <kenny2817.qm@gmail.com>
2024-05-07 00:37:08 +0200

    update verifytoken todo have a better understanding of req.user

--- ours/app/authentication/verifyToken.js
+++ ours/app/authentication/verifyToken.js
@@ -1,16 +1,15 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = function (req, res, next) {
-    const token = req.headers['auth-token'];
-    if (!token) return res.status(401).send('Access denied');
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token == null) return res.sendStatus(401)
 
-    try {
-        const verifyToken = jwt.verify(token, process.env.JWT_SECRET);
-        res.status(200).send(verifyToken);
-        next();
-    } catch (err) {
-        res.status(401).send('Access denied');
-    }
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
 }
 
 // EXAMPLE USE
