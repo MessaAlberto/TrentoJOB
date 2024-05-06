dda093e32fc43941e3dd1baaf3be0c658374bc26
samuele <samuele.camedda@studenti.unitn.it>
2024-05-06 17:48:04 +0200

    prova2

---
+++ ours/static/script/index.js
@@ -0,0 +1,9 @@
+
+
+function signUp() {
+    window.location.href = "registration.html"; // Modifica questo URL con il percorso del tuo file HTML di registrazione
+  }
+  
+  function toggleActive(button) {
+    // Qui puoi aggiungere il codice per attivare/disattivare lo stato del pulsante se necessario
+  }
