0cce7b509c5fc55f58765af5be0c5dc8f694f21b
MessaAlberto <alberto.messa@studenti.unitn.it>
2024-05-11 01:09:26 +0200

    add localStorage

--- ours/static/script/index.js
+++ ours/static/script/index.js
@@ -1,11 +1,43 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Check if user ID is present in localStorage
+    var userId = localStorage.getItem('userId');
+    if (userId) {
+        var username = localStorage.getItem('username');
+        if (username) {
+            displayUserProfile(username);
+        }
+    }
+
+
+    var usernameLabel = document.getElementById('usernameLabel');
+    if (usernameLabel) {
+        usernameLabel.addEventListener('click', function () {
+            window.location.href = "/me.html"; // Redirect to /me page
+        });
+    }
+    var profileIcon = document.getElementById('profileIcon');
+    if (profileIcon) {
+        profileIcon.addEventListener('click', function () {
+            window.location.href = "/me.html"; // Redirect to /me page
+        });
+    }
+});
+
 function signUp() {
-    window.location.href = "registration.html"; // Modifica questo URL con il percorso del tuo file HTML di registrazione
+    window.location.href = "registration.html";
 }
 
 function logIn() {
-  window.location.href = "logIn.html"; // Modifica questo URL con il percorso del tuo file HTML di registrazione
+    window.location.href = "logIn.html";
 }
-  
-function toggleActive(button) {
-    // Qui puoi aggiungere il codice per attivare/disattivare lo stato del pulsante se necessario
+
+
+function displayUserProfile(username) {
+    const singInUp = document.getElementById('sign-in-up');
+    const userProfile = document.getElementById('profile-icon');
+    const usernameLabel = document.getElementById('usernameLabel');
+
+    singInUp.classList.add('hidden');
+    usernameLabel.textContent = username;
+    userProfile.classList.remove('hidden');
 }
