313f283341bd7cd8864e96ab13dec3208e4771ba
samuele <samuele.camedda@studenti.unitn.it>
2024-05-12 21:20:17 +0200

    OpenStreetMap

--- ours/static/script/index.js
+++ ours/static/script/index.js
@@ -9,3 +9,8 @@ function logIn() {
 function toggleActive(button) {
     // Qui puoi aggiungere il codice per attivare/disattivare lo stato del pulsante se necessario
 }
+
+var map = L.map('map').setView([46.06685578571241, 11.118635547295556], 12,8); // Imposta la vista della mappa su una certa posizione e livello di zoom
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { // Aggiunge un layer di mappe di OpenStreetMap
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
