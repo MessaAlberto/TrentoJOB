2230a8a89413c8ce4b397e1e0b00a8b5521971b1
MessaAlberto <alberto.messa@studenti.unitn.it>
2024-05-13 14:58:14 +0200

    add fetch in the homepage

--- ours/static/script/index.js
+++ ours/static/script/index.js
@@ -40,16 +40,16 @@ document.addEventListener('DOMContentLoaded', function () {
             const value = this.value;
             switch (value) {
                 case 'Event':
-                    fetchEvents();
+                    fetchList('Event');
                     break;
                 case 'Announcement':
-                    fetchAnnouncements();
+                    fetchList('Announcement');
                     break;
                 case 'User':
-                    fetchUsers();
+                    fetchList('User');
                     break;
                 case 'Organisation':
-                    fetchOrganisations();
+                    fetchList('Organisation');
                     break;
                 default:
                     // Default case
@@ -105,25 +105,101 @@ function toggleSidebar() {
 }
 
 
+function createItemList(Model, items) {
+    const elementContainer = document.createElement('div');
+    elementContainer.classList.add('element-container');
 
+    const titleElement = document.createElement('h3');
+    titleElement.classList.add('title-container');
+    elementContainer.appendChild(titleElement);
 
+    const titleText = document.createElement('div');
+    titleText.classList.add('title');
+    titleElement.appendChild(titleText);
 
-function fetchEvents() {
+    const expiredText = document.createElement('div');
+    expiredText.classList.add('expired');
+    titleElement.appendChild(expiredText);
 
-    console.log('Event tab selected');
+    const infoContainer = document.createElement('div');
+    infoContainer.classList.add('info-container');
+    elementContainer.appendChild(infoContainer);
+
+    const mainInfo = document.createElement('div');
+    mainInfo.classList.add('main-info');
+    infoContainer.appendChild(mainInfo);
+
+    const dateBegin = document.createElement('div');
+    dateBegin.classList.add('date-begin');
+    mainInfo.appendChild(dateBegin);
+
+    const dateEnd = document.createElement('div');
+    dateEnd.classList.add('date-end');
+    mainInfo.appendChild(dateEnd);
+
+    const location = document.createElement('div');
+    location.classList.add('location');
+    mainInfo.appendChild(location);
+    
+    const owner = document.createElement('div');
+    owner.classList.add('owner');
+    mainInfo.appendChild(owner);
+
+    const email = document.createElement('div');
+    email.classList.add('email');
+    mainInfo.appendChild(email);
+
+    const phone = document.createElement('div');
+    phone.classList.add('phone');
+    mainInfo.appendChild(phone);
+
+    const description = document.createElement('div');
+    description.classList.add('description');
+    infoContainer.appendChild(description);
+
+    if (Model === 'Event' || Model === 'Announcement') {
+        titleText.textContent = items.title;
+        if (items.expired) 
+            expiredText.textContent = 'Expired';
+        location.textContent = items.location;
+        owner.textContent = items.owner.username;
+        if (Model === 'Announcement') {
+            dateBegin.textContent = items.date_begin;
+            dateEnd.textContent = items.date_stop;
+        } else
+            dateBegin.textContent = items.date;
+        description.textContent = items.description;
+    } else if (Model === 'User' || Model === 'Organisation') {
+        titleText.textContent = items.username;
+        description.textContent = items.bio;
+        email.textContent = items.email;
+        phone.textContent = items.phone;
+    }
+
+    console.log(elementContainer);
+    return elementContainer;
 }
 
-function fetchAnnouncements() {
-    // Your code for handling the announcement tab
-    console.log('Announcement tab selected');
-}
+async function fetchList(tabName, title = '') {
+    console.log('Tab selected:', tabName);
 
-function fetchUsers() {
-    // Your code for handling the user tab
-    console.log('User tab selected');
-}
+    const url = '/' + tabName.toLowerCase() + '/?input=' + title;
 
-function fetchOrganisations() {
-    // Your code for handling the organisation tab
-    console.log('Organisation tab selected');
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const items = await response.json();
+        const itemsContainer = document.getElementById('dynamicList');
+        itemsContainer.innerHTML = '';
+
+        items.forEach(item => {
+            const itemElement = createItemList(tabName, item);
+            console.log(itemElement);
+            itemsContainer.appendChild(itemElement);
+        });
+    } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+    }
 }
