c8a9f55046f4bd2f2874cb9520d86ca1997e21c9
MessaAlberto <alberto.messa@studenti.unitn.it>
2024-05-14 10:57:57 +0200

    add createAnnouncement page
    adjust the post request on createEvent page

--- ours/static/script/createAnnouncement.js
+++ ours/static/script/createAnnouncement.js
@@ -1,36 +1,41 @@
 // Handle form submission
 document.addEventListener('DOMContentLoaded', function () {
 
-    document.getElementById('createEventButton').addEventListener('click', function (event) {
+    document.getElementById('createAnnouncementButton').addEventListener('click', function (event) {
         event.preventDefault();
-        createEvent();
+        createAnnouncement();
     });
 });
 
-
-function createEvent() {
+function createAnnouncement() {
 
     // Get the date and time inputs and convert it to a Date object
-    var date = document.getElementById('date').value;
-    var time = document.getElementById('time').value;
-    var isoDateString = date + "T" + time;
-    var dateObject = new Date(isoDateString);
+    var date_begin = document.getElementById('date_begin').value;
+    var time_begin = document.getElementById('time_begin').value;
+    var isoDateString_begin = date_begin + "T" + time_begin;
+    var dateObject_begin = new Date(isoDateString_begin);
+
+    var date_stop = document.getElementById('date_end').value;
+    var time_stop = document.getElementById('time_end').value;
+    var isoDateString_stop = date_stop + "T" + time_stop;
+    var dateObject_stop = new Date(isoDateString_stop);
 
     var formData = {
         title: document.getElementById('title').value,
         description: document.getElementById('description').value,
-        date: dateObject,
+        date_begin: dateObject_begin,
+        date_stop: dateObject_stop,
         location: document.getElementById('location').value,
         maxNumberParticipants: document.getElementById('max-participants').value
     };
 
     // Create a new XMLHttpRequest
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", "/event", true);
+    xhr.open("POST", "/announcement", true);
     xhr.setRequestHeader("Content-Type", "application/json");
     // Add Authorization header
     xhr.setRequestHeader("Authorization", "Bearer " + localStorage.token);
-
+    
     // Manage the response
     xhr.onload = function () {
         if (xhr.status === 201) {
@@ -38,7 +43,7 @@ function createEvent() {
             var response = JSON.parse(xhr.responseText);
             window.location.href = "/me.html";
         } else {
-            alert("An error occurred during event creation.");
+            alert("An error occurred during announcement creation.");
         }
     };
 
