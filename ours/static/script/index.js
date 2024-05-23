7a5cdf96480ac4fa55717fb6bae6255b063291c9
MessaAlberto <alberto.messa@studenti.unitn.it>
2024-05-23 15:52:23 +0200

    add frontend filters/sorting

--- ours/static/script/index.js
+++ ours/static/script/index.js
@@ -1,9 +1,13 @@
 let map;
 let radioButtonsValue = 'event';
+let jsonQuery = {};
 
 document.addEventListener('DOMContentLoaded', function () {
 
-    displayCreateButton();
+    const filterForm = document.getElementById('filterForm');
+    filterForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+    });
 
     // show Trento on the map
     map = L.map('map').setView([46.066422, 11.125760], 13);
@@ -19,19 +23,19 @@ document.addEventListener('DOMContentLoaded', function () {
     radioButtons.forEach(radioButton => {
         radioButton.addEventListener('click', function () {
             radioButtonsValue = this.value;
-            // Empty the search bar
-            document.getElementById('searchInput').value = '';
-            // Fetch the list of items and display them on the map
-
-            fetchList(radioButtonsValue);
-            // Display filter
             createFilterForm();
+            createOrderingOption();
+            fetchList(radioButtonsValue);
+
+            document.getElementById('searchInput').value = '';
         });
     });
 
+    displayCreateButton();
+    createFilterForm();
+    createOrderingOption();
 
     fetchList('event');
-    createFilterForm();
 });
 
 
@@ -49,8 +53,7 @@ async function getCoordinatesFromLocation(location) {
     }
 }
 
-
-async function fetchAndDisplayItems(model, items) {
+async function displayItemsOnMap(model, items) {
     try {
         // clean the map
         map.eachLayer(function (layer) {
@@ -93,26 +96,93 @@ async function fetchAndDisplayItems(model, items) {
     }
 }
 
+
+
 function searchInputBar(event) {
     if (event.keyCode === 13) {
         event.preventDefault();
-        const title = document.getElementById('searchInput').value.trim();
-        if (title) {
-            const query = { input: title };
-            fetchList(radioButtonsValue, query);
-        }
-        document.getElementById('searchInput').value = '';
+        fetchList(radioButtonsValue);
     }
 }
 
 function searchButton() {
+    fetchList(radioButtonsValue);
+}
+
+function orderBy() {
+    fetchList(radioButtonsValue);
+}
+
+function resetFilters() {
+    jsonQuery = {};
+    createFilterForm();
+}
+
+
+
+async function fetchList(model) {
+    try {
+        jsonQuery = {};
+        addOrderToQuery();
+        addInputToQuery();
+        addFiltersToQuery();
+
+        console.log(jsonQuery);
+
+        const queryString = new URLSearchParams(jsonQuery).toString();
+        const url = '/' + model + '?' + queryString;
+
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const items = await response.json();
+        const itemsContainer = document.getElementById('dynamicList');
+        itemsContainer.innerHTML = '';
+
+        items.forEach(item => {
+            const itemElement = createItemList(model, item);
+            itemsContainer.appendChild(itemElement);
+        });
+
+        // show markers on the map
+        displayItemsOnMap(model, items);
+    } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+    }
+}
+
+
+
+function addInputToQuery() {
     const title = document.getElementById('searchInput').value.trim();
     if (title) {
-        const query = { input: title };
-        fetchList(radioButtonsValue, query);
+        jsonQuery = { ...jsonQuery, input: title };
     }
-    document.getElementById('searchInput').value = '';
 }
+
+function addOrderToQuery() {
+    const ordering = document.getElementById('orderBy');
+    const selected = ordering.options[ordering.selectedIndex].value;
+    const [field, order] = selected.split(':');
+    jsonQuery.sortBy = `${field}:${order}`;
+}
+
+function addFiltersToQuery() {
+    const form = document.getElementById('filterForm');
+    const formData = new FormData(form);
+
+    formData.forEach((value, key) => {
+        if (value) {
+            jsonQuery[key] = value;
+        }
+    });
+    console.log(jsonQuery);
+}
+
+
+
 
 function displayCreateButton() {
     // Only logged in users can create notices
@@ -143,6 +213,7 @@ function createActivity() {
         alert("You must be logged in to create a notice.");
 
 }
+
 
 
 function createItemList(model, item) {
@@ -329,31 +400,259 @@ function createItemList(model, item) {
     return elementContainer;
 }
 
-async function fetchList(model, query = {}) {
-    try {
-        const queryString = new URLSearchParams(query).toString();
-        const url = '/' + model + '?' + queryString;
+function createOrderingOption() {
+    const ordering = document.getElementById('orderBy');
+    ordering.innerHTML = '';
 
-        const response = await fetch(url);
+    const options = [];
 
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
+    if (radioButtonsValue === 'user' || radioButtonsValue === 'organisation') {
+        options.push({ value: 'username:asc', text: 'Username: A-Z' });
+        options.push({ value: 'username:desc', text: 'Username: Z-A' });
+    }
+
+    if (radioButtonsValue === 'event' || radioButtonsValue === 'announcement') {
+        options.push({ value: 'title:asc', text: 'Title: A-Z' });
+        options.push({ value: 'title:desc', text: 'Title: Z-A' });
+        options.push({ value: 'date:asc', text: 'Date: Soonest first' });
+        options.push({ value: 'date:desc', text: 'Date: Latest first' });
+    }
+
+    if (radioButtonsValue === 'user') {
+        options.push({ value: 'birthday:asc', text: 'Age: Youngest first' });
+        options.push({ value: 'birthday:desc', text: 'Age: Oldest first' });
+    }
+
+    options.push({ value: 'score:asc', text: 'Score: Low to High' });
+    options.push({ value: 'score:desc', text: 'Score: High to Low' });
+
+    options.forEach((option, index) => {
+        const opt = document.createElement('option');
+        opt.value = option.value;
+        opt.innerHTML = option.text;
+
+        // Set the first option as selected by default
+        if (index === 0) {
+            opt.selected = true;
         }
-        const items = await response.json();
-        const itemsContainer = document.getElementById('dynamicList');
-        itemsContainer.innerHTML = '';
 
-        items.forEach(item => {
-            const itemElement = createItemList(model, item);
-            itemsContainer.appendChild(itemElement);
+        ordering.appendChild(opt);
+    });
+}
+
+function createFilterForm() {
+    const filterContainer = document.getElementById('filterContainer');
+    filterContainer.innerHTML = '';
+
+    // Create the score filter with star score
+    const score = document.createElement('div');
+    const scoreLabel = document.createElement('label');
+    scoreLabel.innerHTML = 'Score:';
+    score.appendChild(scoreLabel);
+
+    const scoreDiv = document.createElement('div');
+    scoreDiv.classList.add('score');
+
+    for (let i = 5; i >= 1; i--) {
+        const starLabel = document.createElement('label');
+        const starInput = document.createElement('input');
+        starInput.setAttribute('type', 'radio');
+        starInput.setAttribute('name', 'score');
+        starInput.setAttribute('value', i);
+        starInput.setAttribute('title', `${i} stars`);
+        starLabel.appendChild(starInput);
+        starLabel.appendChild(document.createTextNode(` ${i} `));
+        scoreDiv.appendChild(starLabel);
+    }
+
+    score.appendChild(scoreDiv);
+    filterContainer.appendChild(score);
+
+    if (radioButtonsValue === 'user') {
+        // Create user age filter
+        const age = document.createElement('div');
+        const ageLabel = document.createElement('label');
+        ageLabel.setAttribute('for', 'age');
+        ageLabel.innerHTML = 'Age:';
+        age.appendChild(ageLabel);
+
+        const minAgeInput = document.createElement('input');
+        minAgeInput.setAttribute('type', 'number');
+        minAgeInput.setAttribute('id', 'minAge');
+        minAgeInput.setAttribute('name', 'minAge');
+        minAgeInput.setAttribute('placeholder', 'Min');
+        minAgeInput.setAttribute('min', '0');
+        minAgeInput.setAttribute('max', '120');
+        age.appendChild(minAgeInput);
+
+        const maxAgeInput = document.createElement('input');
+        maxAgeInput.setAttribute('type', 'number');
+        maxAgeInput.setAttribute('id', 'maxAge');
+        maxAgeInput.setAttribute('name', 'maxAge');
+        maxAgeInput.setAttribute('placeholder', 'Max');
+        maxAgeInput.setAttribute('min', '0');
+        maxAgeInput.setAttribute('max', '120');
+        age.appendChild(maxAgeInput);
+
+        filterContainer.appendChild(age);
+
+        minAgeInput.addEventListener('change', function () {
+            if (parseInt(minAgeInput.value) > parseInt(maxAgeInput.value)) {
+                minAgeInput.value = maxAgeInput.value;
+            }
         });
 
-        // show markers on the map
-        fetchAndDisplayItems(model, items);
-    } catch (error) {
-        console.error('There has been a problem with your fetch operation:', error);
+        maxAgeInput.addEventListener('change', function () {
+            if (parseInt(maxAgeInput.value) < parseInt(minAgeInput.value)) {
+                maxAgeInput.value = minAgeInput.value;
+            }
+        });
+
+        // Create sex filter
+        const sex = document.createElement('div');
+        const sexLabel = document.createElement('label');
+        sexLabel.setAttribute('for', 'sex');
+        sexLabel.innerHTML = 'Sex: ';
+        sex.appendChild(sexLabel);
+
+        // Create selcet dropdown
+        const sexSelect = document.createElement('select');
+        sexSelect.setAttribute('id', 'sex');
+        sexSelect.setAttribute('name', 'sex');
+
+        // Create options for "All", "Male", and "Female"
+        const sexOption = document.createElement('option');
+        sexOption.setAttribute('value', '');
+        sexOption.textContent = 'All';
+        sexSelect.appendChild(sexOption);
+
+        const optionMale = document.createElement('option');
+        optionMale.setAttribute('value', 'male');
+        optionMale.textContent = 'Male';
+        sexSelect.appendChild(optionMale);
+
+        const optionFemale = document.createElement('option');
+        optionFemale.setAttribute('value', 'female');
+        optionFemale.textContent = 'Female';
+        sexSelect.appendChild(optionFemale);
+
+        sex.appendChild(sexSelect);
+        filterContainer.appendChild(sex);
+    } else if (radioButtonsValue === 'event' || radioButtonsValue === 'announcement') {
+        const expired = document.createElement('div');
+        const expiredLabel = document.createElement('label');
+        expiredLabel.setAttribute('for', 'expired');
+        expiredLabel.innerHTML = 'Expired:';
+        expired.appendChild(expiredLabel);
+
+        // Create select dropdown
+        const expiredSelect = document.createElement('select');
+        expiredSelect.setAttribute('id', 'expired');
+        expiredSelect.setAttribute('name', 'expired');
+
+        // Create options for "Yes" and "No"
+        const expiredOption = document.createElement('option');
+        expiredOption.setAttribute('value', '');
+        expiredOption.textContent = 'All';
+        expiredSelect.appendChild(expiredOption);
+
+        const optionYes = document.createElement('option');
+        optionYes.setAttribute('value', 'true');
+        optionYes.textContent = 'Yes';
+        expiredSelect.appendChild(optionYes);
+
+        const optionNo = document.createElement('option');
+        optionNo.setAttribute('value', 'false');
+        optionNo.textContent = 'No';
+        expiredSelect.appendChild(optionNo);
+
+        expired.appendChild(expiredSelect);
+        filterContainer.appendChild(expired);
+
+        // Create the max number of participants filter
+        const maxNumberParticipants = document.createElement('div');
+        const maxNumberParticipantsLabel = document.createElement('label');
+        maxNumberParticipantsLabel.setAttribute('for', 'maxNumberParticipants');
+        maxNumberParticipantsLabel.innerHTML = 'Max num. participants:';
+        maxNumberParticipants.appendChild(maxNumberParticipantsLabel);
+        const maxNumberParticipantsInput = document.createElement('input');
+        maxNumberParticipantsInput.setAttribute('type', 'number');
+        maxNumberParticipantsInput.setAttribute('id', 'maxNumberParticipants');
+        maxNumberParticipantsInput.setAttribute('name', 'maxNumberParticipants');
+        maxNumberParticipantsInput.setAttribute('min', '0');
+        maxNumberParticipants.appendChild(maxNumberParticipantsInput);
+        filterContainer.appendChild(maxNumberParticipants);
+
+        if (radioButtonsValue === 'event') {
+            // Create single date picker
+            const date = document.createElement('div');
+            const dateLabel = document.createElement('label');
+            dateLabel.innerHTML = 'Date:';
+            date.appendChild(dateLabel);
+
+            const dateInput = document.createElement('input');
+            dateInput.setAttribute('type', 'date');
+            dateInput.setAttribute('name', 'date');
+            date.appendChild(dateInput);
+            filterContainer.appendChild(date);
+        } else {
+            // Create the date range picker
+            const dateRangePicker = document.createElement('div');
+            const dateRangeLabel = document.createElement('label');
+            dateRangeLabel.innerHTML = 'Date Range:';
+            dateRangePicker.appendChild(dateRangeLabel);
+
+            const dateRangeInput = document.createElement('input');
+            dateRangeInput.setAttribute('type', 'text');
+            dateRangeInput.setAttribute('name', 'daterange');
+            dateRangePicker.appendChild(dateRangeInput);
+            filterContainer.appendChild(dateRangePicker);
+        }
     }
+
+    // jQuery
+    // score
+    $('.score input').change(function () {
+        var $radio = $(this);
+        $('.score .selected').removeClass('selected');
+        $radio.closest('label').addClass('selected');
+    });
+
+    // date range
+    $('input[name="daterange"]').daterangepicker({
+        autoUpdateInput: false,
+        locale: {
+            format: 'MM/DD/YYYY - MM/DD/YYYY',
+        }
+    });
+
+    // Update the input value on apply event
+    $('input[name="daterange"]').on('apply.daterangepicker', function(ev, picker) {
+        $(this).val(picker.startDate.format('DD/MM/YYYY') + ' - ' + picker.endDate.format('DD/MM/YYYY'));
+    });
+
+    // Clear the input value on cancel event
+    $('input[name="daterange"]').on('cancel.daterangepicker', function(ev, picker) {
+        $(this).val('');
+    });
+
+
+    // single date
+    $('input[name="date"]').daterangepicker({
+        autoUpdateInput: false,
+        locale: {
+            format: 'YYYY-MM-DD',
+        },
+        singleDatePicker: true,
+        showDropdowns: true,
+    },
+        function (start, end, label) {
+            $('input[name="date"]').val(start.format('YYYY-MM-DD'));
+            $(this).val(start.format('YYYY-MM-DD'));
+        });
 }
+
+
 
 async function fecthObject(model, ownerId) {
     const url = '/' + model + '/' + ownerId;
@@ -390,101 +689,4 @@ async function fecthObject(model, ownerId) {
     } catch (error) {
         console.error('There has been a problem with your fetch operation:', error);
     }
-}
-
-function createFilterForm() {
-    const filterContainer = document.getElementById('filterContainer');
-    filterContainer.innerHTML = '';
-
-    const expired = document.createElement('div');
-    const expiredLabel = document.createElement('label');
-    expiredLabel.setAttribute('for', 'expired');
-    expiredLabel.innerHTML = 'Expired:';
-    expired.appendChild(expiredLabel);
-    const expiredInput = document.createElement('input');
-    expiredInput.setAttribute('type', 'checkbox');
-    expiredInput.setAttribute('id', 'expired');
-    expiredInput.setAttribute('name', 'expired');
-    expiredInput.setAttribute('value', 'true');
-    expired.appendChild(expiredInput);
-    filterContainer.appendChild(expired);
-
-    // Create the score filter with star rating
-    const score = document.createElement('div');
-    const scoreLabel = document.createElement('label');
-    scoreLabel.setAttribute('for', 'score');
-    scoreLabel.innerHTML = 'Score:';
-    score.appendChild(scoreLabel);
-
-    const ratingDiv = document.createElement('div');
-    ratingDiv.classList.add('rating');
-
-    for (let i = 5; i >= 1; i--) {
-        const starLabel = document.createElement('label');
-        const starInput = document.createElement('input');
-        starInput.setAttribute('type', 'radio');
-        starInput.setAttribute('name', 'rating');
-        starInput.setAttribute('value', i);
-        starInput.setAttribute('title', `${i} stars`);
-        starLabel.appendChild(starInput);
-        starLabel.appendChild(document.createTextNode(` ${i} `));
-        ratingDiv.appendChild(starLabel);
-    }
-
-    score.appendChild(ratingDiv);
-    filterContainer.appendChild(score);
-
-
-    const dateBegin = document.createElement('div');
-    const dateBeginLabel = document.createElement('label');
-    dateBeginLabel.setAttribute('for', 'dateBegin');
-    dateBeginLabel.innerHTML = 'Date begin:';
-    dateBegin.appendChild(dateBeginLabel);
-    const dateBeginInput = document.createElement('input');
-    dateBeginInput.setAttribute('type', 'date');
-    dateBeginInput.setAttribute('id', 'dateBegin');
-    dateBeginInput.setAttribute('name', 'dateBegin');
-    dateBegin.appendChild(dateBeginInput);
-    filterContainer.appendChild(dateBegin);
-
-    const dateEnd = document.createElement('div');
-    const dateEndLabel = document.createElement('label');
-    dateEndLabel.setAttribute('for', 'dateEnd');
-    dateEndLabel.innerHTML = 'Date end:';
-    dateEnd.appendChild(dateEndLabel);
-    const dateEndInput = document.createElement('input');
-    dateEndInput.setAttribute('type', 'date');
-    dateEndInput.setAttribute('id', 'dateEnd');
-    dateEndInput.setAttribute('name', 'dateEnd');
-    dateEnd.appendChild(dateEndInput);
-    filterContainer.appendChild(dateEnd);
-
-    const maxNumberParticipants = document.createElement('div');
-    const maxNumberParticipantsLabel = document.createElement('label');
-    maxNumberParticipantsLabel.setAttribute('for', 'maxNumberParticipants');
-    maxNumberParticipantsLabel.innerHTML = 'Max number participants:';
-    maxNumberParticipants.appendChild(maxNumberParticipantsLabel);
-    const maxNumberParticipantsInput = document.createElement('input');
-    maxNumberParticipantsInput.setAttribute('type', 'number');
-    maxNumberParticipantsInput.setAttribute('id', 'maxNumberParticipants');
-    maxNumberParticipantsInput.setAttribute('name', 'maxNumberParticipants');
-    maxNumberParticipants.appendChild(maxNumberParticipantsInput);
-    filterContainer.appendChild(maxNumberParticipants);
-
-
-}
-
-
-function applayFilter() {
-    const form = document.getElementById('filterForm');
-    const formData = new FormData(form);
-    const filters = {};
-
-    formData.forEach((value, key) => {
-        if (value) {
-            filters[key] = value;
-        }
-    });
-
-    fetchList(radioButtonsValue, filters);
 }
