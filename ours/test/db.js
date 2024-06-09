0a7b9df7c98332419138ea281e9473801aa36c7a
MessaAlberto <alberto.messa@studenti.unitn.it>
2024-06-09 02:18:27 +0200

    tested user.js

---
+++ ours/test/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
+let mongoServer;
+
+async function connect() {
+    mongoServer = await MongoMemoryServer.create();
+    const uri = mongoServer.getUri();
+    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+}
+
+async function disconnect() {
+    await mongoose.disconnect();
+    await mongoServer.stop();
+}
+
+module.exports = { connect, disconnect };
